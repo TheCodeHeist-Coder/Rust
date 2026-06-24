4118ef8303cd0bb7dc4b79d5fb769ae336c0308e
Rajkumar <thecodeheist01@gmail.com>
2026-06-24 23:15:46 +0530

    rust basics 01

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,47 @@
 
+
+
 fn main(){
-    println!("Hello Raj");
+
+    //! Variables
+    // let x: i32 = -2;  // here i32 is a signed integer of 32 bits (+ or -)
+    // let y: u32 = 20;  // u32 is an unsigned integer
+    // let z: f32 = 12.32; // number can be + , - or decimal
+
+    // print!(
+    //     "x: {}, y: {}, z: {}", x, y, z
+    // )
+
+    //? ___________OR_________
+
+    // print!("x: {}", x);
+    // print!("y: {}", y);
+    // print!("z: {}", z);
+
+
+    //! booleans
+        // let is_male = true;
+        // let is_above_18 = true;
+
+        // if is_male {
+        //     println!("You are a male");
+        // } else {
+        //     println!("You are not a male")
+        // }
+
+        // if is_male && is_above_18 {
+        //     println!("You are good to go!")
+        // }
+
+
+    //! strings
+        let name: String = String::from("Hello! Raj");
+        println!("{}", name);
+
+        //? print the first letter
+         // Se'll later
+         
+
+
+    
 }
