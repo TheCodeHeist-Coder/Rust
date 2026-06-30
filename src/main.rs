52f024392c0b915b344716c1021181c4c1f98c3c
Rajkumar <thecodeheist01@gmail.com>
2026-07-01 00:50:09 +0530

    implement structs done

--- src/main.rs
+++ src/main.rs
@@ -306,4 +306,28 @@
 
     // }
 
-    //!
+    //! implementing structs
+    
+    struct Rect {
+        width: u32,
+        height: u32,
+    }
+
+    impl Rect {
+        fn area(&self) -> u32 {
+            self.width * self.height
+        }
+
+        fn parimeter(&self) -> u32 {
+            2 * (self.width + self.height)
+        }
+    }
+
+    fn main(){
+        let rect = Rect {
+            width: 30,
+            height: 50,
+        };
+        print!("The area of the rectangle is {}", rect.area());
+        print!("The area of the rectangle is {}", rect.parimeter());
+    }
