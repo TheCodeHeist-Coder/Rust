90509325b5d8ce00be0c5cce17b82aa19d6ea931
Rajkumar <thecodeheist01@gmail.com>
2026-07-22 22:56:58 +0530

    all about functions from the rust book

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,63 @@
-fn main(){
-    println!("Hello Raj");
-    my_function();
-}
 
-fn my_function(){
-    println!("Hii this is my function");
-}
+
+//! Normal function declaration & calling
+// fn main(){
+//     println!("Hello Raj");
+//     my_function();
+// }
+
+// fn my_function(){
+//     println!("Hii this is my function");
+// }
+
+
+//! function parameters
+// fn main(){
+//     println!("Function parameters!");
+//     func_param(4, true);
+// }
+
+// fn func_param(x:i32, y:bool) {
+//     println!("This function have the value {} and it is {}", x, y);
+// }
+
+
+
+//! Statement
+// fn main(){
+    
+    //? Statements
+    // let x:i32 = 10;
+    // println!("Hello this is the {}", x);
+
+    // let x: i32 = 20;
+    // let y: i32 = 20;
+    // let add: i32 = x + y;
+    // println!("Sum is {}", add);
+
+// }
+
+//! expressions
+    // fn main(){
+
+    //     let y: i32 = {
+    //         let x: i32 = 5;
+    //         x + 5        //? this is the expression because it doesn't contain semicolon
+    //     };
+
+    //     println!("The value of Y is: {}",y);
+    // }
+
+
+
+    //! functions with return values
+    
+    // fn main(){
+    //     println!("Hii we are learning..");
+    //    let x = print_five();
+    //    println!("value is: {}",x);
+    // }
+
+    // fn print_five() -> i32 {
+    //     10
+    // }
